086dc803df3319881d82f04917e43604596b0d5b
Abdul Gany Osman <Abdul.Osman@bbd.co.za>
2024-07-24 09:16:46 +0200

    First test code

---
+++ lambda/books_by_genre.ts
@@ -0,0 +1,27 @@
+import {type APIGatewayProxyHandler} from 'aws-lambda';
+import {getNeptuneConnection} from '../utils/dbUtils';
+import * as gremlin from 'gremlin';
+
+export const handler: APIGatewayProxyHandler = async (event) => {
+    const {driverConnection, graph} = getNeptuneConnection();
+    const statics = gremlin.process.statics;
+
+    try {
+        const genre = event.pathParameters?.genre;
+        const genreNode = await graph.V().has('name', genre);
+        const output = await graph.V(genreNode).in_().hasLabel('Book').values('title').toList();
+        await driverConnection.close();
+        console.log(output);
+        return {
+            statusCode: 200,
+            body: JSON.stringify(output)
+        };
+    } catch (e) {
+        await driverConnection.close();
+        console.log(e);
+        return {
+            statusCode: 500,
+            body: JSON.stringify(e)
+        };
+    }
+};
---
+++ lambda/suggestion_books_by_genre.ts
@@ -0,0 +1,27 @@
+import {type APIGatewayProxyHandler} from 'aws-lambda';
+import {getNeptuneConnection} from '../utils/dbUtils';
+import * as gremlin from 'gremlin';
+
+export const handler: APIGatewayProxyHandler = async (event) => {
+    const {driverConnection, graph} = getNeptuneConnection();
+    const statics = gremlin.process.statics;
+
+    try {
+        const userId = event.pathParameters?.userId;
+        const readList = await graph.V(userId).out().hasLabel('Book').id();
+        const output = await graph.V(userId).out().hasLabel('Book').out().hasLabel('Genre').dedup().in_().hasLabel('Book').hasId(!('b-1')).limit(3).values('title').toList();
+        await driverConnection.close();
+        console.log(output);
+        return {
+            statusCode: 200,
+            body: JSON.stringify(output)
+        };
+    } catch (e) {
+        await driverConnection.close();
+        console.log(e);
+        return {
+            statusCode: 500,
+            body: JSON.stringify(e)
+        };
+    }
+};
