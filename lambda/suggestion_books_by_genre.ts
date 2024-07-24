9fe249a02d6910a8cd7a84d8e07c23098e5c16e1
Abdul Gany Osman <Abdul.Osman@bbd.co.za>
2024-07-24 10:39:12 +0200

    Test

--- lambda/suggestion_books_by_genre.ts
+++ lambda/suggestion_books_by_genre.ts
@@ -11,7 +11,7 @@ export const handler: APIGatewayProxyHandler = async (event) => {
         console.log(userId);
         const readList = await graph.V(userId).out().hasLabel('Book').id().toList();
         console.log(readList);
-        const output = await graph.V(userId).out().hasLabel('Book').out().hasLabel('Genre').dedup().in_().hasLabel('Book').hasId(!('b-1')).limit(3).values('title').toList();
+        const output = await graph.V(userId).out().hasLabel('Book').out().hasLabel('Genre').dedup().in_().hasLabel('Book').hasId('b-1').limit(3).values('title').toList();
         await driverConnection.close();
         console.log(output);
         return {
