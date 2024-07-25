fda7501c68081efb06fa4c4946deed88379564b5
Abdul Gany Osman <Abdul.Osman@bbd.co.za>
2024-07-25 10:16:53 +0200

    added create endpoints

---
+++ lambda/create_reader_to_book.ts
@@ -0,0 +1,27 @@
+import {type APIGatewayProxyHandler} from 'aws-lambda';
+import {getNeptuneConnection} from '../utils/dbUtils';
+import * as gremlin from 'gremlin';
+
+export const handler: APIGatewayProxyHandler = async (event) => {
+    const {driverConnection, graph} = getNeptuneConnection();
+    const statics = gremlin.process.statics;
+
+    try {
+        const { book, reader} = JSON.parse(event.body || '{}');
+        const relationship = await graph.addE('has-read').from_(reader).to(book);
+        await driverConnection.close();
+        const output = { relationships: relationship}
+        console.log(output);
+        return {
+            statusCode: 200,
+            body: JSON.stringify(output)
+        };
+    } catch (e) {
+        await driverConnection.close();
+        console.log(e);
+        return {
+            statusCode: 500,
+            body: JSON.stringify(e)
+        };
+    }
+};
